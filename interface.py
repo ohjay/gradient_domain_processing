44360ca7aca5930a8c978447774f7cedfa88d6b5
Owen Jow <owenjow@berkeley.edu>
2018-12-25 12:26:52 -0800

    clean up drag/drop

--- interface.py
+++ interface.py
@@ -78,9 +78,6 @@ def lasso(image):
         alpha_mask.flat[ind] = 1.0
         alpha_mask = alpha_mask[ymin:ymax, xmin:xmax]
         output = np.dstack((output, alpha_mask))
-        # [for rectangular source region]
-        # alpha_mask = np.ones((ymax-ymin, xmax-xmin))
-        # output = np.dstack((image[ymin:ymax, xmin:xmax], alpha_mask))
 
         ax.clear()
         ax.imshow(selected)
@@ -102,96 +99,38 @@ def lasso(image):
 # GDF SOURCE PLACEMENT #
 ########################
 
-def drag_layer(source, target):
-    """Drag-and-drop interface for adding source image to target image."""
-    assert source.shape[-1] == 4  # assume alpha channel
-    assert target.shape[-1] == 3  # assume no alpha channel
-    assert np.less(source.shape[:-1], target.shape[:-1]).all()
-
-    if source.dtype == np.uint8:
-        source = source / 255.
-    if target.dtype == np.uint8:
-        target = target / 255.
-
-    th, tw = target.shape[:2]
-    sh, sw = source.shape[:2]
-
-    ey, ex = None, None  # event position
-    sy, sx = 0, 0        # source position (top left)
-    source_canvas = np.zeros((th, tw, 4))
-    source_canvas[sy:sy+sh, sx:sx+sw] = source
-
-    TITLE = 'Press ENTER when satisfied with your placement.'
-    fig = plt.figure()
-    plt.tick_params(axis='both', which='both', bottom='off', top='off',
-                    labelbottom='off', right='off', left='off', labelleft='off')
-    ax = fig.add_subplot(111)
-    ax.imshow(target)
-    ax.imshow(source_canvas)
-    ax.set_title(TITLE)
-
-    def onpress(event):
-        if None in (event.ydata, event.xdata):
-            return True
-        nonlocal ey, ex
-        ey = event.ydata
-        ex = event.xdata
-        return True
-
-    def onrelease(event):
-        if None in (event.ydata, event.xdata):
-            return True
-        nonlocal sy, sx
-        sy = np.clip(sy + int(event.ydata - ey), 0, th - sh)
-        sx = np.clip(sx + int(event.xdata - ex), 0, tw - sw)
-        source_canvas[:] = 0
-        source_canvas[sy:sy+sh, sx:sx+sw] = source
-
-        ax.clear()
-        ax.imshow(target)
-        ax.imshow(source_canvas)
-        ax.set_title(TITLE)
-        fig.canvas.draw_idle()
-        return True
-
-    def quit_figure(event):
-        # Source: https://github.com/matplotlib/matplotlib/issues/830/.
-        if event.key == 'enter':
-            plt.close(event.canvas.figure)
-
-    gcf = plt.gcf()
-    cid = gcf.canvas.mpl_connect('key_press_event', quit_figure)
-    did = gcf.canvas.mpl_connect('button_press_event', onpress)
-    eid = gcf.canvas.mpl_connect('button_release_event', onrelease)
-    plt.show()
-    return sy, sx
-
-
-
-# ______ NEW ______
-
-
-
 class DragGuiTk(tk.Frame):
+
+    MAX_CANVAS_DIM = 750
 
     def __init__(self, root, source_path, target_path):
         self.root = root
         tk.Frame.__init__(self, root)
 
-        canvas_width = 500
-        canvas_height = 400
-        self.canvas = tk.Canvas(self, width=canvas_width, height=canvas_height)
+        source = PIL.Image.open(source_path)
+        target = PIL.Image.open(target_path)
+        sw, sh = source.size
+        tw, th = target.size
+        _scale = DragGuiTk.MAX_CANVAS_DIM / float(tw if tw > th else th)
+        source = source.resize((int(sw * _scale), int(sh * _scale)))
+        target = target.resize((int(tw * _scale), int(th * _scale)), PIL.Image.ANTIALIAS)
+        assert sw < tw and sh < th  # also source should have alpha channel, target should not
 
-        basewidth = 500
-        img = PIL.Image.open(target_path)
-        wpercent = (basewidth / float(img.size[0]))
-        hsize = int((float(img.size[1]) * float(wpercent)))
-        self.target_image = PIL.ImageTk.PhotoImage(img.resize((basewidth, hsize), PIL.Image.ANTIALIAS))
-        self.source_image = PIL.ImageTk.PhotoImage(file=source_path)
-        # Need to use `create_image` for transparency
-        self.target_label = self.canvas.create_image(0, 0, image=self.target_image)
-        self.source_label = self.canvas.create_image(0, 0, image=self.source_image)
-        self.canvas.pack(side='top', fill='both', anchor='c')
+        self.sw = sw
+        self.sh = sh
+        self.tw = tw
+        self.th = th
+        self.scale = _scale
+        self.canvas_w = int(tw * _scale)
+        self.canvas_h = int(th * _scale)
+        self.canvas = tk.Canvas(self, width=self.canvas_w, height=self.canvas_h)
+
+        self.target_image = PIL.ImageTk.PhotoImage(target)
+        self.source_image = PIL.ImageTk.PhotoImage(source)
+        # Use `create_image` for transparency
+        self.target_label = self.canvas.create_image(0, 0, image=self.target_image, anchor='nw')
+        self.source_label = self.canvas.create_image(0, 0, image=self.source_image, anchor='nw')
+        self.canvas.pack()
         self.canvas.bind('<Button-1>', self.onclick)
         self.canvas.bind('<ButtonRelease-1>', self.onrelease)
 
@@ -218,6 +157,14 @@ class DragGuiTk(tk.Frame):
     def onrelease(self, event):
         self.canvas.unbind('<B1-Motion>', self.motion_id)
 
+    def pp_sx(self):
+        sx = int(self.sx / self.scale)
+        return np.clip(sx, 0, self.tw - self.sw)
+
+    def pp_sy(self):
+        sy = int(self.sy / self.scale)
+        return np.clip(sy, 0, self.th - self.sh)
+
 def drag_layer(source_path, target_path):
     root = tk.Tk()
     root.title('Close window when satisfied with your placement.')
@@ -227,7 +174,7 @@ def drag_layer(source_path, target_path):
     gui.pack()
 
     root.mainloop()
-    return gui.sy, gui.sx
+    return gui.pp_sy(), gui.pp_sx()
 
 #########
 # DEBUG #
