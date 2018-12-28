fcb7188e20d823e2577256885466c769943e454d
Owen Jow <owenjow@berkeley.edu>
2018-12-28 00:44:26 -0800

    simplify logic a little

--- blend.py
+++ blend.py
@@ -45,69 +45,81 @@ def blend(source, target, location, mixed_gradients=False):
     source = source.astype(np.float32)
     target = target.astype(np.float32)
 
+    # Mask out border of source region
+    # Avoids having to do boundary checks later
+    source[0,  :, -1] = 0
+    source[-1, :, -1] = 0
+    source[:,  0, -1] = 0
+    source[:, -1, -1] = 0
+
     start_time = time.time()
-    th, tw = target.shape[:2]
     sh, sw = source.shape[:2]
     ly, lx = location
-    num_px = (sh - 2) * (sw - 2)
+    num_px = sh * sw
 
     def ravel(y, x):
-        """Convert unraveled (sh, sw) coordinates to flat (sh - 2, sw - 2) index."""
-        return (y - 1) * (sw - 2) + (x - 1)
+        """Convert unraveled (sh, sw) coordinates to flat index."""
+        return y * sw + x
 
+    A_data, A_i, A_j, b = [], [], [], []
+    eqn_idx = 0
     for ch in range(3):
-        A_data, A_i, A_j, b = [], [], [], []
-        eqn_idx = 0
-        for y in range(1, sh - 1):
-            for x in range(1, sw - 1):
+        for y in range(sh):
+            for x in range(sw):
                 # Check alpha channel
                 if source[y, x, -1] > 0:
-                    Aval = 0
                     bval = 0
                     for dy, dx in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
                         ny = y + dy  # neighboring y
                         nx = x + dx  # neighboring x
+                        source_grad = source[y, x, ch] - source[ny, nx, ch]
+                        target_grad = target[ly + y, lx + x, ch] - target[ly + ny, lx + nx, ch]
+                        if mixed_gradients and abs(target_grad) > abs(source_grad):
+                            bval += target_grad
+                        else:
+                            bval += source_grad
                         if source[ny, nx, -1] > 0:
-                            Aval += 1
-                            # mixed gradients
-                            source_grad = source[y, x, ch] - source[ny, nx, ch]
-                            target_grad = target[ly + y, lx + x, ch] - target[ly + ny, lx + nx, ch]
-                            if mixed_gradients and abs(target_grad) > abs(source_grad):
-                                bval += target_grad
-                            else:
-                                bval += source_grad
-                            ny2 = y + 2 * dy  # 2 away
-                            nx2 = x + 2 * dx  # in gradient direction
-                            if ny2 < 0 or nx2 < 0 or ny2 >= sh \
-                                    or nx2 >= sw or source[ny2, nx2, -1] == 0:
-                                # On edge of valid source region in grad direction
-                                bval += target[ly + ny, lx + nx, ch]
-                            else:
-                                A_data.append(-1)
-                                A_i.append(eqn_idx)
-                                A_j.append(ravel(ny, nx))
-                    A_data.append(Aval)
+                            A_data.append(-1)
+                            A_i.append(eqn_idx)
+                            A_j.append(ch * num_px + ravel(ny, nx))
+                        else:
+                            bval += target[ly + ny, lx + nx, ch]
+                    A_data.append(4)
                     A_i.append(eqn_idx)
-                    A_j.append(ravel(y, x))
+                    A_j.append(ch * num_px + ravel(y, x))
                     b.append(bval)
                 else:
                     # Not part of source, constrain to be target value
                     A_data.append(1)
                     A_i.append(eqn_idx)
-                    A_j.append(ravel(y, x))
+                    A_j.append(ch * num_px + ravel(y, x))
                     b.append(target[ly + y, lx + x, ch])
                 eqn_idx += 1  # equation for every pixel
-        A_data = np.array(A_data)
-        A_i = np.array(A_i)
-        A_j = np.array(A_j)
-        A = sparse.coo_matrix((A_data, (A_i, A_j)), shape=(num_px, num_px))
-        A = sparse.csc_matrix(A)
-        v = sparse.linalg.lsqr(A, b, iter_lim=1e5)[0]
-        v = np.clip(np.reshape(v, (sh - 2, sw - 2)), 0.0, 1.0)
-        target[ly+1:ly+sh-1, lx+1:lx+sw-1, ch] = v  # paste region into target
-    print('Time elapsed: %.4fs' % (time.time() - start_time))
+
+    print('for loop | Time elapsed: %.4fs' % (time.time() - start_time))
+    start_time = time.time()
+
+    A_data = np.array(A_data)
+    A_i = np.array(A_i)
+    A_j = np.array(A_j)
+    A = sparse.coo_matrix((A_data, (A_i, A_j)), shape=(num_px * 3, num_px * 3))
+    A = sparse.csc_matrix(A)
+    v = sparse.linalg.lsqr(A, b, iter_lim=1e5)[0]
+
+    v = np.stack((
+        np.reshape(v[:num_px], (sh, sw)),
+        np.reshape(v[num_px:2*num_px], (sh, sw)),
+        np.reshape(v[2*num_px:], (sh, sw))), axis=-1)
+    target[ly:ly+sh, lx:lx+sw] = np.clip(v, 0.0, 1.0)  # paste region into target
+
+    print('lsqr sol | Time elapsed: %.4fs' % (time.time() - start_time))
     plt.imshow(target)
-    plt.show()
+    while True:
+        try:
+            plt.show()
+        except UnicodeDecodeError:
+            continue
+        break
     misc.imsave('images/blended.png', target)
     print('Saved result to `images/blended.png`.')
 
